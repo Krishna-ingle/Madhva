31ff8c670555bbeea41e4f74a4b3f903bfb1c7ed
Krishna Ingle <129488148+Krishna-ingle@users.noreply.github.com>
2026-07-03 08:48:58 +0530

    Add NetBillPrivacyPolicy component

---
+++ src/components/Netbillprivacypolicy.jsx
@@ -0,0 +1,7 @@
+// src/components/NetBillPrivacyPolicy.jsx
+import React from 'react';
+import LegalDocPage from './LegalDocPage';
+
+const NetBillPrivacyPolicy = () => <LegalDocPage docType="privacy" />;
+
+export default NetBillPrivacyPolicy;
