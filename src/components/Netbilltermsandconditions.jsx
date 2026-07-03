4c3946180058e80904a2a14dccd19b723c8d2c2a
Krishna Ingle <129488148+Krishna-ingle@users.noreply.github.com>
2026-07-03 08:49:52 +0530

    Add NetBillTermsAndConditions component

---
+++ src/components/Netbilltermsandconditions.jsx
@@ -0,0 +1,7 @@
+// src/components/NetBillTermsAndConditions.jsx
+import React from 'react';
+import LegalDocPage from './LegalDocPage';
+
+const NetBillTermsAndConditions = () => <LegalDocPage docType="terms" />;
+
+export default NetBillTermsAndConditions;
